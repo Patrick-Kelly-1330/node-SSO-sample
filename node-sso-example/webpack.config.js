1a0893ee1c58ded3efb3dd330d52daa30e55ca40
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-04-18 22:23:09 -0700

    update webpack config to prepare for deployment

---
+++ node-sso-example/webpack.config.js
@@ -0,0 +1,56 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+var SRC_DIR = path.join(__dirname, "/routes");
+var DIST_DIR = path.join(__dirname, "/client/dist");
+
+
+export default {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use:{
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader"}
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ejs$/,
+        use: [
+            {
+              loader: "ejs-webpack-loader",
+              options: {
+                data: {title: "New Title", someVar:"hello world"},
+                htmlmin: true
+              }
+            }
+        ]
+    }
+    ]
+  }
+};
