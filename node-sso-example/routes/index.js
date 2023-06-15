e0c4deae0a69b2d092bac7e1a28aca6d4c918fa5
Patrick Kelly <patrickkelly@Patricks-MacBook-Pro.local>
2023-06-15 13:34:08 -0700

    implement directory sync in same app

--- node-sso-example/routes/index.js
+++ node-sso-example/routes/index.js
@@ -19,6 +19,7 @@ const clientID = process.env.WORKOS_CLIENT_ID
 const organizationID = 'org_01H2M21QBQPDG1KNCD3FGE7W2W' 
 const redirectURI = 'http://localhost:8000/callback'
 const state = ''
+const directoryGroup = 'directory_group_01H30BCQG77NACW58VPVKYX2TV'
 
 router.get('/', function (req, res) {
     if (session.isloggedin) {
@@ -58,7 +59,7 @@ router.get('/callback', async (req, res) => {
         })
 
         const groupInfo = await workos.directorySync.listUsers({
-            group: 'directory_group_01GY1N3B9RDY35SJ3X0DRMAHV5',
+            group: directoryGroup, 
         })
 
         const json_profile = JSON.stringify(profile, null, 4)
@@ -69,18 +70,6 @@ router.get('/callback', async (req, res) => {
         session.firstPersonFirst = groupInfo.data[0].first_name
         session.firstPersonLast = groupInfo.data[0].last_name
         session.firstPersonEmail = groupInfo.data[0].username
-        session.secondPersonFirst = groupInfo.data[1].first_name
-        session.secondPersonLast = groupInfo.data[1].last_name
-        session.secondPersonEmail = groupInfo.data[1].username
-        session.thirdPersonFirst = groupInfo.data[2].first_name
-        session.thirdPersonLast = groupInfo.data[2].last_name
-        session.thirdPersonEmail = groupInfo.data[2].username
-        session.fourthPersonFirst = groupInfo.data[3].first_name
-        session.fourthPersonLast = groupInfo.data[3].last_name
-        session.fourthPersonEmail = groupInfo.data[3].username
-        session.fifthPersonFirst = groupInfo.data[4].first_name
-        session.fifthPersonLast = groupInfo.data[4].last_name
-        session.fifthPersonEmail = groupInfo.data[4].username
         res.redirect('/')
     } catch (error) {
         res.render('error.ejs', { error: error })
